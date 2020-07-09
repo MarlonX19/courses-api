08b10224310b6e5203402b1b56181cac3497e57a
marlonx19 <marlon.englemam@gmail.com>
2020-07-09 13:56:53 -0300

    feat(Controllers): added new http methods

--- src/controllers/CourseController.js
+++ src/controllers/CourseController.js
@@ -24,10 +24,33 @@ module.exports = {
       return res.json(courses);
     }
     catch (error) {
-      return res.status(500).json({ message: "Erro interno, tente mais tarde!" });
+      return res.status(500).json({ message: "Erro interno buscar, tente mais tarde!!" });
     }
 
   },
+
+
+  async findCourse(req, res) {
+    const { q } = req.query;
+
+    try {
+      const courses = await Course.find({ 'title': new RegExp(q, 'i') });
+      console.log(courses);
+
+      if (courses.length > 0) {
+        return res.json(courses);
+      }
+
+      return res.json({ message: 'Nenhum curso encontrado para sua busca' })
+
+
+    }
+    catch (error) {
+      return res.status(500).json({ message: "Erro interno, tente mais tarde!!!" });
+    }
+
+  },
+
 
 
   async store(req, res) {
@@ -40,7 +63,7 @@ module.exports = {
     }
 
     try {
-      const crs = await Course.creates({
+      const crs = await Course.create({
         title,
         subtitle,
         startedAt,
@@ -54,6 +77,51 @@ module.exports = {
 
   },
 
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    try {
+      const course = await Course.find({ _id: id });
+
+      if (course.length > 0) {
+        const delCourse = await Course.deleteOne({ _id: id });
+
+        return res.status(204);
+      }
+
+      return res.status(404).json({ message: 'Curso não encontrado, impossível deletar' });
+
+    }
+    catch (error) {
+      return res.status(500).json({ message: "Erro interno ao tentar deletar curso, tente mais tarde!!" });
+    }
+
+  },
+
+
+  async update(req, res) {
+    const { id } = req.params;
+    const { title, subtitle, startedAt, description } = req.body;
+
+    try {
+      const course = await Course.find({ _id: id });
+
+      if (course.length > 0) {
+        const upCourse = await Course.findOneAndUpdate({ _id: id }, { title, subtitle, startedAt, description });
+
+        return res.status(200).json({ upCourse });
+      }
+
+      return res.status(404).json({ message: 'Curso não encontrado, impossível atualizar' });
+
+    }
+    catch (error) {
+      return res.status(500).json({ message: "Erro interno ao tentar atualizar curso, tente mais tarde!!" });
+    }
+
+
+  }
 
 }
 
--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,14 @@ const routes = express.Router();
 
 
 routes.get('/courses', Course.index);
+routes.get('/courses/find', Course.findCourse);
 routes.get('/courses/:id', Course.listCourse);
 
 
+
 routes.post('/courses', Course.store);
+routes.delete('/courses/:id', Course.delete);
+routes.put('/courses/:id', Course.update);
 
 
 
